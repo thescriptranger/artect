3c2d9fc3b110330abed45282936e4082d409000c
Art Laubach <art.laubach@gmail.com>
2026-04-22 22:59:47 -0600

    feat(generation): wire emitter registry (batch 9H)

---
+++ src/Artect.Generation/EmitterRegistry.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Artect.Generation.Emitters;
+
+namespace Artect.Generation;
+
+public static class EmitterRegistry
+{
+    public static IReadOnlyList<IEmitter> All() => new IEmitter[]
+    {
+        new ApiProblemEmitter(),
+        new AppSettingsEmitter(),
+        new ArtectConfigEmitter(),
+        new CsProjEmitter(),
+        new DapperConnectionFactoryEmitter(),
+        new DapperRepositoryImplEmitter(),
+        new DbContextEmitter(),
+        new DbFunctionsEmitter(),
+        new DockerEmitter(),
+        new DtoEmitter(),
+        new EfRepositoryEmitter(),
+        new EntityEmitter(),
+        new EnumEmitter(),
+        new LaunchSettingsEmitter(),
+        new MapperEmitter(),
+        new MigrationsEmitter(),
+        new MinimalApiEndpointEmitter(),
+        new PagedResponseEmitter(),
+        new ProgramCsEmitter(),
+        new RepoHygieneEmitter(),
+        new RepositoryInterfaceEmitter(),
+        new RequestEmitter(),
+        new ResponseEmitter(),
+        new SlnEmitter(),
+        new StoredProceduresEmitter(),
+        new TestsProjectEmitter(),
+        new ValidationErrorEmitter(),
+        new ValidationResultEmitter(),
+        new ValidatorEmitter(),
+    };
+}
